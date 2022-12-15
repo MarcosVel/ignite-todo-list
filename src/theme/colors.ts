4c35d8bdcd0b688f2d9593ccca9f83d97018dd14
MarcosVel <marcos.veloso@sempreceub.com>
2022-12-14 23:55:02 -0300

    feat: add header and images

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  gray700: "#0D0D0D",
+  gray600: "#1A1A1A",
+  gray500: "#262626",
+  gray400: "#333333",
+  gray300: "#808080",
+  gray200: "#D9D9D9",
+  gray100: "#F2F2F2",
+
+  blue: "#4EA8DE",
+  blueDark: "#1E6F9F",
+
+  purple: "#8284FA",
+};
