54cff4b09ea2e374144241f468da72c582b5edda
MarcosVel <marcos.veloso@sempreceub.com>
2022-12-15 01:07:45 -0300

    feat: input styles

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, StyleSheet, Image } from "react-native";
-import { COLORS } from "./src/theme/colors";
-import logo from "./src/assets/logo.png";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "./src/assets/logo.png";
+import { COLORS } from "./src/theme/colors";
+import { Feather } from "@expo/vector-icons";
 
 export default function App() {
   return (
@@ -11,6 +19,17 @@ export default function App() {
       <Image source={logo} style={styles.logo} />
 
       <View style={styles.main}>
+        <View style={styles.add}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor={COLORS.gray300}
+          />
+          <TouchableOpacity style={styles.addButton}>
+            <Feather name="plus-circle" size={20} color={COLORS.gray100} />
+          </TouchableOpacity>
+        </View>
+
         <Text style={[styles.empty, { fontWeight: "bold" }]}>
           Open up App.tsx to teste working on your app!
         </Text>
@@ -34,8 +53,32 @@ const styles = StyleSheet.create({
   },
   main: {
     flex: 1,
+    paddingHorizontal: 24,
     backgroundColor: COLORS.gray600,
     alignItems: "center",
+  },
+  add: {
+    flexDirection: "row",
+    marginTop: -28,
+    marginBottom: 32,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: COLORS.gray500,
+    padding: 16,
+    fontSize: 16,
+    color: COLORS.gray100,
+    borderRadius: 6,
+    maxHeight: 55,
+    marginRight: 4,
+  },
+  addButton: {
+    width: 54,
+    height: 54,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.blueDark,
+    borderRadius: 6,
   },
   empty: {
     color: COLORS.gray300,
